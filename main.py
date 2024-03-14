4b48c03d0f76096bfd20d940ecc9c33ef20cc878
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-14 15:27:22 -0400

    Add driver for Service + ChromeDriverManager

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
