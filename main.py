5ddf262e5a0b514e2c3804d19740f3fe940081da
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-14 15:28:59 -0400

    Add sleep intervals + functional steps for app

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ import time
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.amazon.com/")
+
+time.sleep(2)
+search = driver.find_element(By.XPATH, "/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input")
+search.send_keys("house of the dragon")
+search.send_keys(Keys.RETURN)
+time.sleep(2)
+driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div/div[2]/div/div/div[1]/h2/a/span").click()
+time.sleep(3)
+driver.save_screenshot("/Users/administrator/Desktop/HouseOfDragon.png")
+time.sleep(5)
